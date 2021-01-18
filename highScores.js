360e8e8e2dfb15cb9ba18493809b679ba3147856
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-18 14:03:58 -0800

    event listener to return to index page. added retriever for locally stored scores and a clear for scores

--- highScores.js
+++ highScores.js
@@ -10,3 +10,18 @@ clear.addEventListener("click", function () {
 });
 //Retrieves scores the are stored in local storage
 var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].userScore;
+        highScores.appendChild(createLi);
+
+    }
+}
+
+//event listener to listen for users click to return to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html")
+})
