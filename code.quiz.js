8e9bdea62af51f7f83ae8ce16676b392d13e3f67
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-18 12:27:25 -0800

    finished questions. started variable for working code

--- code.quiz.js
+++ code.quiz.js
@@ -12,14 +12,40 @@ var quizQuestions = [
         question: "What is a very useful tool that is used during development, debugging and for printing content to the debugger:",
         choices: ["Javascript", "Terminal / bash", "For loops", "console.log()"],
         answer: "console.log()"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ____.",
+        choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+        answer: "Parentheses"
+    },
+    {
+        question: "What does CSS stand for:",
+        choices: ["Code Style Sheet", "Cascading Style Sheet", "Coding Scripted Style", "Cryptic Styling Sheet"],
+        answer: "Cascading Style Sheet"
+    },
+    {
+        question: "String Values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+];
+//Declared var for userScore and questionsIndex
+var userScore = 0;
+var questionIndex = 0;
 
+var = currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startQuiz");
+var questionsDiv = document.querySelector("#questionsDiv");
+var container = document.querySelector("#container");
 
-
-
-
-
-
-]
+//Timer of 20 seconds per questions
+var = secondsLeft = 100;
+//Holds interval time
+var holdInterval = 0;
+//Holds penalty time
+var penalty = 5;
+//Creates new element for ul choices list
+var ulCreate = document.createElement("ul");
 
 
 
