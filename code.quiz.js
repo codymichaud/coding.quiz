72d04cec46ca17531d45a64472494606bcd2fd23
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-17 20:24:37 -0800

    starting functionality of quiz. coding javascript on code.quiz

---
+++ code.quiz.js
@@ -0,0 +1,22 @@
+//10 questions 10 points each
+//Lose 5 points or seconds for each wrong answer
+
+var quizQuestions = [
+    {
+        question: "Arrays in Javascript can be used to store ______:",
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+
+    },
+
+
+
+
+
+
+
+]
+
+
+
+
